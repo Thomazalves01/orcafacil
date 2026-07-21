bf051efb393d52546956a7e47643262e43f41394
Thomazalves01 <thomaz.fifa2000@gmail.com>
2026-07-21 12:19:41 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+/* ==========================================================
+   OrçaFácil
+   Configurações Globais
+========================================================== */
+
+const CONFIG = {
+
+    APP_NAME: "OrçaFácil", 
+
+    VERSION: "0.1.0",
+
+    STORAGE_KEYS: {
+
+        COMPANY: "orcafacil-company",
+
+        CLIENT: "orcafacil-client",
+
+        ITEMS: "orcafacil-items",
+
+        SETTINGS: "orcafacil-settings"
+
+    }
+
+};
