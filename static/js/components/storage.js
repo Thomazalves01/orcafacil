468dd6edc6e72172d847b45b35ebc428444c3196
Thomazalves01 <thomaz.fifa2000@gmail.com>
2026-07-21 19:17:55 -0300

    Create storage.js

---
+++ static/js/components/storage.js
@@ -0,0 +1,22 @@
+/* ==========================================================
+   OrçaFácil
+   Componente: Storage
+========================================================== */
+
+const Storage = {
+
+    save(key, data) {
+
+        localStorage.setItem(key, JSON.stringify(data));
+
+    },
+
+    load(key) {
+
+        const data = localStorage.getItem(key);
+
+        return data ? JSON.parse(data) : null;
+
+    }
+
+};
